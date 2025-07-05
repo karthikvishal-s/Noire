3a82fcb937005fe2dc4fd0f0950f5d3f7061c618
Karthik Vishal <karthikvishal1506@gmail.com>
2025-07-05 13:18:52 +0530

    Added metadata for title

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,13 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
-
+import {assets} from "@/assets/assets";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  title: "Noire",
+  description: "E-Commerce Store for Fashion and Lifestyle",
+ 
 };
 
 export default function RootLayout({ children }) {
